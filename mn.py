26c7a0241fe2168b44ccf38f4843ce11c0223a6e
ManchikantiNehanjali <84323699+ManchikantiNehanjali@users.noreply.github.com>
2023-03-25 15:50:35 +0530

    Update mn.py

--- mn.py
+++ mn.py
@@ -1,13 +1,1 @@
-s=[int(x) for x in input().split()]
-arr=[]
-for i in s:
-    arr.append(str(i))
-for i in range(len(arr)):
-    key=i
-    for j in range(i+1):
-        if arr[j]+arr[key]<arr[key]+arr[j]:
-            arr[key],arr[j]=arr[j],arr[key]
-            key=i
-print(arr)
-            
-    
+
